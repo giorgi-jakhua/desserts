2a6817bfb385ee170aaefc16fa44fab35c9d8f0a
giorgi-jakhua <jaxuagiorgi5@gmail.com>
2024-12-12 00:56:52 +0400

    small design changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,23 @@ const BASE_URL ="https://res.cloudinary.com/dc2c49xov/desserts/"
 function App() {
   return (
     <>
-      <h1>hello world <AddToCart /></h1>
-      
-      {dessertsProducts.map((dessert) => {
-        return (
-          <div key={dessert.name}>
-            <img src={`${BASE_URL}/${dessert.images.thumbnail}`} alt="" />
-          </div>
-        )
-      })}
+      <h1>Desserts</h1>
+        
+      <div className="desserts-container">
+        {dessertsProducts.map((dessert) => {
+          return (
+            <div className='dessert-box' key={dessert.name}>
+              <div className="dessert-img-box">
+                <img src={`${BASE_URL}/${dessert.images.desktop}`} alt="" />
+                <button className='add-to-cart-btn'> <AddToCart /> add to cart</button>
+              </div>
+              <p>{dessert.name}</p>
+              <h4>{dessert.category}</h4>
+              <h3>{dessert.price.toFixed(2)}$</h3>
+            </div>
+          )
+        })}
+      </div>
     </>
     
   )
